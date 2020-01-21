941d4fd27a3cfed65757246ed3b462b01dd6fa20
AlejandroManzano <alex.batt99@gmail.com>
2020-01-20 23:37:41 -0600

    Creación interacción DAO - Servicio, fin video 11

--- src/main/java/com/manzano/mensajes/MensajesDao.java
+++ src/main/java/com/manzano/mensajes/MensajesDao.java
@@ -1,9 +1,40 @@
 package com.manzano.mensajes;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+/**
+ * @author Alex Manz
+ * */
 public class MensajesDao {
-
-    public static void crearMensajeDB(MensajesDO mensajesDO){
-
+    /**
+     * CRUD de mensajes contenido y autor de los mensajes
+     * Aqui viven las operaciones tal cual se operan en la DB
+     * */
+    public static void crearMensajeDB(MensajesDO mensajes)
+    /**
+     * @Param mensajes contenido y autor del mensaje
+     * Operación de generación de una nueva fila.
+     * */
+    {
+        ConexionDb conexionDb = new ConexionDb();
+        try(Connection connection = conexionDb.getConexion()) {
+            PreparedStatement ps = null;
+            try{
+                /*Llenando la consulta con el contenido de mensajes*/
+                String consulta ="INSERT INTO `mensajes` (`mensaje`, `autor_mensaje`) VALUES (?,?)";
+                ps = connection.prepareStatement(consulta);
+                ps.setString(1, mensajes.getMensaje());
+                ps.setString(2, mensajes.getAutorMensaje());
+                ps.executeUpdate();
+                System.out.println("mensaje creado");
+            }catch (SQLException ex){
+                System.out.println(ex);
+            }
+        }catch (SQLException e){
+            System.out.printf("");
+        }
     }
 
     public static void litarMensajesDB(){
--- src/main/java/com/manzano/mensajes/MensajesServicio.java
+++ src/main/java/com/manzano/mensajes/MensajesServicio.java
@@ -1,8 +1,24 @@
 package com.manzano.mensajes;
 
-public class MensajesServicio {
+import java.util.Scanner;
 
+public class MensajesServicio {
+    /**
+     * @Autor Alex Manz
+     * Aqui se manejan los datos entre el lado del cliente y la base de datos,
+     * se preparan para ser manejados por la capa DAO e interactuar con  la
+     * DB
+     * */
     public static void crearMensaje(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Escribel el mensaje");
+        String mensaje = sc.nextLine();
+        System.out.println("Escribe tu nombre");
+        String autor = sc.nextLine();
+        MensajesDO registro = new MensajesDO();
+        registro.setMensaje(mensaje);
+        registro.setAutorMensaje(autor);
+        MensajesDao.crearMensajeDB(registro);
 
     }
 
